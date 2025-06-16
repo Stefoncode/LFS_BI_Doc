51ccf74a0d65c4fde79e9fe8cc3ddfc09e3d3750
Stéphane Liabeuf <stephane.liabeuf@gmail.com>
2025-06-16 11:46:04 +0200

    Ajout des maquettes

---
+++ docs/Elements/Icons/empty.md
@@ -0,0 +1,5 @@
+# Pictogram
+
+![Mockup](/MockupWireframe/BiSalesCockpit_Desktop_Dashboard_Winter_Offers_Mockup_V01.png)
+
+# ScoreCard
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -101,7 +101,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      links: [
+/*       links: [
         {
           title: 'Docs',
           items: [
@@ -141,8 +141,8 @@ const config: Config = {
             },
           ],
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      ], */
+      copyright: `Copyright © ${new Date().getFullYear()} LFS Design System for PowerBi.`,
     },
     prism: {
       theme: prismThemes.github,
